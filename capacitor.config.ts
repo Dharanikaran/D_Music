6c17013bb8aea7390edad2c8222d55676fa5593c
Dharanikaran R <149926016+Dharanikaran@users.noreply.github.com>
2026-07-13 15:42:05 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.plug.musicapp',
-  appName: 'Plug',
-  webDir: 'www'
-};
-
-export default config;
